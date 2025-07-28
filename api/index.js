d16548dff0c79b2de9dd8531d021b8d07045ac73
AnilKumarBanoth <an5689247@gmail.com>
2025-07-28 20:04:55 +0530

    updated one

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
-const app = require('../index'); // Import the Express app
-const serverless = require('serverless-http');
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
 
+// middlewares
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello from Vercel Serverless!");
+});
+
+// Export the handler
 module.exports.handler = serverless(app);
